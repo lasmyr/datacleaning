0bebf6fdf5148b72a53e5c26590b63b927c23fc9
Lasmyr <ldedullantes@globe.com.ph>
2019-04-16 08:13:47 +0800

    Revised to add activity labels

--- run_analysis.R
+++ run_analysis.R
@@ -1,32 +1,39 @@
-# Merges the training and the test sets to create one data set.
-# Extracts only the measurements on the mean and standard deviation for each measurement.
-# Uses descriptive activity names to name the activities in the data set
-# Appropriately labels the data set with descriptive variable names.
-# From the data set in step 4, creates a second, independent tidy data set 
-# with the average of each variable for each activity and each subject.
-
 #X = measurements; Y = Activity
+#Load libraries
+library(dplyr)
 
-#Read Train Data
+#Read all the data necessary
 x_train<-read.table("~/Data Cleaning/train/X_train.txt")
 y_train<-read.table("~/Data Cleaning/train/Y_train.txt")
 subject_train<-read.table("~/Data Cleaning/train/subject_train.txt")
 
-#Read Test Data
 x_test<-read.table("~/Data Cleaning/test/X_test.txt")
 y_test<-read.table("~/Data Cleaning/test/Y_test.txt")
 subject_test<-read.table("~/Data Cleaning/test/subject_test.txt")
 
-#Read Descriptions
 var_name<-read.table("~/Data Cleaning/features.txt")
+activity_labels<-read.table("~/Data Cleaning/activity_labels.txt")
 
-#Merge Trial and Train data
+
+#Merge test and train data
 X_all<-rbind(x_train, x_test)
 Y_all<-rbind(y_train, y_test)
 subject_all<-rbind(subject_train, subject_test)
 
-#Select mean and st. dev for each measurements
 
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-selected_var <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
-X_total <- X_total[,selected_var[,1]]
+#Select only the mean and the st. dev
+selectedvar<-var_name[grep("mean\\(\\)|std\\(\\)",var_name[,2]),]
+X_all<-X_all[,selectedvar[,1]]
+
+
+#Descriptive names for the measurements
+colnames(X_all)<-selectedvar[,2]
+colnames(Y_all)<-"activity"
+colnames(subject_all)<-"subject"
+mergeddf<-cbind(subject_all,X_all,Y_all)
+head(mergeddf)
+
+
+#Dataset arranged by subject and activity
+totalmean <- mergeddf %>% group_by(activity, subject) %>% summarize_all(funs(mean)) 
+write.table(totalmean, file = "~/Data Cleaning/datacleaning/tidydata.txt", row.names = FALSE, col.names = TRUE) 
